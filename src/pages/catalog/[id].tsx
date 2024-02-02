a6b66304bb26b72885e8eec653ceca94b4ace76f
Davide Rossi <davide.rossi@dieffe.tech>
2024-02-02 17:27:57 +0100

    create detail ssr

--- src/pages/catalog/[id].tsx
+++ src/pages/catalog/[id].tsx
@@ -1,11 +1,57 @@
-import { useRouter } from "next/router";
+//import { useRouter } from "next/router";
 
-export default function CatalogDetails() {
-  const router = useRouter();
+import { Gadget } from "@/models/gadget";
+import axios from "axios";
+import { GetServerSidePropsContext } from "next";
+import Image from "next/image";
+import Link from "next/link";
 
+//use my-json-server
+const API =
+  "https://my-json-server.typicode.com/training-api/next-course-gadgets/gadgets";
+
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  // use '?' after params because value is possible a NULL
+  const id = context.params?.id;
+
+  try {
+    const { data } = await axios.get<Gadget>(`${API}/${id}`);
+    return {
+      props: { data },
+    };
+  } catch (err) {
+    return {
+      notFound: true,
+    };
+  }
+};
+
+// export default function CatalogDetails() {
+//   const router = useRouter();
+
+//   return (
+//     <>
+//       <h1 className="title-pink">Catalog Details di {router.query.id}</h1>
+//     </>
+//   );
+// }
+
+interface CatalogDetailProps {
+  data: Gadget;
+}
+
+export default function CatalogDetails({ data }: CatalogDetailProps) {
   return (
     <>
-      <h1 className="title-pink">Catalog Details di {router.query.id}</h1>
+      <h1 className="title-pink">{data.title}</h1>
+
+      <Image src={data.image} alt={data.title} width={300} height={300} />
+
+      <Link href="/catalog" legacyBehavior>
+        <a className="text-pink-500 mt-3">Back</a>
+      </Link>
     </>
   );
 }
--- src/pages/catalog/index.tsx
+++ src/pages/catalog/index.tsx
@@ -1,7 +1,74 @@
-export default function Index() {
+import Link from "next/link";
+import Head from "next/head";
+import axios from "axios";
+import { Gadget } from "@/models/gadget";
+import Image from "next/image";
+import { InferGetServerSidePropsType } from "next";
+import { notFound } from "next/navigation";
+
+//use my-json-server
+const API =
+  "https://my-json-server.typicode.com/training-api/next-course-gadgets/gadgets";
+
+export const getServerSideProps = async () => {
+  try {
+    const { data } = await axios.get<Gadget[]>(API);
+    return {
+      props: { data },
+    };
+  } catch (err) {
+    return {
+      notFound: true,
+
+      // permanent true or false
+      // if true will use the 308 status code which instructs clients/search engines to cache the redirect forever
+      // if false will use the 307 status code which is temporary and is not cached.
+
+      // in this case, we redirect to home page if API is wrong or not exist
+      // redirect: {
+      //   destination: "/",
+      //   permanent: true
+      // },
+    };
+  }
+};
+
+// use interface to define type of props, then wrap type of params into function with CatalogIndexProps
+// interface CatalogIndexProps {
+//   data: Gadget[];
+// }
+
+export default function Index({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <>
-      <h1 className="title-pink">Catalog list</h1>
+      <Head>
+        <title>Catalog</title>
+      </Head>
+
+      <h1 className="title-pink">Catalog</h1>
+
+      <div className="flex">
+        {data.map((item) => {
+          return (
+            <Link key={item.id} href={`/catalog/${item.id}`} legacyBehavior>
+              <a href="">
+                <Image
+                  src={item.image}
+                  alt={item.title}
+                  width={300}
+                  height={300}
+                />
+              </a>
+            </Link>
+          );
+        })}
+      </div>
+
+      <Link href="/catalog/123" legacyBehavior>
+        <a className="text-pink-500 mt-3">Go to product</a>
+      </Link>
     </>
   );
 }
